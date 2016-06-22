9bd8af9fbdb86682d74dff7711fb2f3d784e05e3
Kimberly Lewis <kblewis.flt@gmail.com>
2016-06-21 21:18:51 -0500

    big commit

---
+++ letterInterview/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letterInterview
+{
+    class Program
+    {
+        static void Main()
+        {
+            string lines;
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(@"C:\Users\owner\Documents\Visual Studio 2013\Projects\letterInterview\letterInterview\InterviewFile.csv"))
+            {
+                lines = reader.ReadToEnd();
+            }
+            //System.IO.StreamReader lines = new System.IO.StreamReader(@"C:\Users\owner\Documents\Visual Studio 2013\Projects\letterInterview\letterInterview\InterviewFile.csv");
+            char[] delimiterChar = { ',' };
+            String[] words = lines.Split(delimiterChar);
+            foreach (string s in words)
+            {
+                System.Console.WriteLine(s);
+            }
+            System.Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
